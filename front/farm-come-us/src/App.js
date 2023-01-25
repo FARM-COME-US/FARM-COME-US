b46c9587fa3d89f1f3f83d0f2610cb2bce0c31ff
kevin-Arpe <arpeggiogio@gmail.com>
2023-01-25 17:21:15 +0900

    modify: 기본 react 컴포넌트 작성

--- front/farm-come-us/src/App.js
+++ front/farm-come-us/src/App.js
@@ -1,8 +1,25 @@
-import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
-  return <div className="App">App</div>;
-}
+import { Routes, Route } from "react-router-dom";
+
+import Header from "./components/common/Header";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/SignUp";
+import Cart from "./pages/Cart";
+
+const App = () => {
+  return (
+    <div id="app">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/sign-up" element={<Signup />}></Route>
+        <Route path="/cart" element={<Cart />}></Route>
+      </Routes>
+    </div>
+  );
+};
 
 export default App;
