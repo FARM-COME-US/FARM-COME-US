d9b7d0e7817989ec0b51356d7fefa764ad8c39ce
차현경 <h30young@naver.com>
2023-01-25 15:57:35 +0900

    modify: root package name

---
+++ back/farmcu/src/main/java/com/ssafy/farmcu/entity/live/LiveLike.java
@@ -0,0 +1,30 @@
+package com.ssafy.farmcu.entity.live;
+
+import com.ssafy.farmcu.entity.member.Member;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "live_like")
+public class LiveLike {
+    //필드
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "live_like_id")
+    private Long id;
+
+    // 외래 키
+    @ManyToOne(cascade = CascadeType.MERGE, targetEntity = Member.class)
+    @JoinColumn(name = "member_id", updatable = false)
+    private Member member;
+
+    @ManyToOne(cascade = CascadeType.MERGE, targetEntity = Live.class)
+    @JoinColumn(name = "live_id", updatable = false)
+    private Live live;
+
+}
+
