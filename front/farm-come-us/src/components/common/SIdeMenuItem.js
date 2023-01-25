53b3f624972950b877d38d97faae7ba3fcd405b4
CessnaJ <eon8748@gmail.com>
2023-01-26 02:26:29 +0900

    add #S08P12B103-252 SideMenuItem

--- front/farm-come-us/src/components/common/SIdeMenuItem.js
+++ front/farm-come-us/src/components/common/SIdeMenuItem.js
@@ -1,17 +1,18 @@
 import React from "react";
 import classes from "./style/SideMenuItem.module.scss";
+import { useNavigate } from "react-router-dom";
 
 const SideMenuItem = (props) => {
   const isLogin = ""; // 유저정보 구독해야함
+  navigate = useNavigate();
+
   return (
-    <div className={classes.SideMenuItem}>
+    <div className={classes.SideMenuItem} onClick={navigate(props.linkTo)}>
+      {/* 상위 컴포넌트에서 prop으로 주는 주소로 들어가게 했음. */}
       <div>
         <image src={`./baseURLforimage/${props.imageName}`} alt="" />
         <div>{props.itemName}</div>
       </div>
-
-      {isLogin ? <div className={classes.SideMenuItem}></div> : ""}
-      {/* 로그아웃버튼 -> 로그인시 렌더링, 아니면 안함. */}
     </div>
   );
 };
--- front/farm-come-us/src/components/common/SideMenu.js
+++ front/farm-come-us/src/components/common/SideMenu.js
@@ -1,8 +1,14 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { userSlice } from "../../reduxStore/userSlice";
+import SideMenuItem from "./SIdeMenuItem";
 import classes from "./style/SideMenu.module.scss";
 
 const SideMenu = (props) => {
-  const isLogin = "";
+  const user = useSelector((state) => state.user.value); // 로그인상태가 따라 화면 재렌더링
+  const isLogin = user.nickname; // 일단 간이로 nickname받아오면 로그인된걸로 설정
+  const dispatch = useDispatch();
+
   return (
     <div>
       <div>
@@ -14,17 +20,30 @@ const SideMenu = (props) => {
             <div>이메일</div>
           </div>
         ) : (
-          <div>로그인</div>
+          <div>
+            <div>기본 프로필사진</div>
+            <div>로그인</div>
+          </div>
         )}
       </div>
-      <div>
-        <div className={classes.SideMenuItem}></div>
 
-        <div className={classes.SideMenuItem}></div>
-        <div className={classes.SideMenuItem}></div>
+      <div>
+        <SideMenuItem linkTo="" imageName="" itemName="" />
+        <SideMenuItem linkTo="" imageName="" itemName="" />
+        <SideMenuItem linkTo="" imageName="" itemName="" />
       </div>
 
-      {isLogin ? <div className={classes.SideMenuItem}></div> : ""}
+      {isLogin ? (
+        <div
+          className={classes.SideMenuItem}
+          onClick={() => {
+            dispatch(userSlice.actions.logout());
+            // 로그아웃 바로 되지 않고, 로그아웃할거냐고 물어보고, 확인 누르면 해야될거같음.
+          }}
+        ></div>
+      ) : (
+        ""
+      )}
       {/* 로그아웃버튼 -> 로그인시 렌더링, 아니면 안함. */}
     </div>
   );
