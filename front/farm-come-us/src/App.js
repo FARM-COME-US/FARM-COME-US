c9b878728ad5f6ec6891aebdadaa2bc1e4839532
CessnaJ <eon8748@gmail.com>
2023-01-27 03:34:55 +0900

    chore: 사이드바 닫을때 backdrop 유지되는 문제점 주석

--- front/farm-come-us/src/App.js
+++ front/farm-come-us/src/App.js
@@ -34,7 +34,7 @@ const App = () => {
 
         {isOpen ? (
           <div>
-            {/* <Backdrop /> 사이드바 컨텐츠가 없는거였음. 버그 아님. */}
+            {/* <Backdrop /> 사이드바 컨텐츠가 없는거였음. 버그 아님. 클릭했을때 app.js에 영향을 못미쳐서 사이드바만 들어가는거였음. redux통해 관리필요. */}
             <SideMenu className="sideMenu" />
           </div>
         ) : (
