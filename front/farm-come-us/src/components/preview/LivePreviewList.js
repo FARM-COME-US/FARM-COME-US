00baa8672a9c047296435a8caf463635efc65cbd
kevin-Arpe <arpeggiogio@gmail.com>
2023-01-26 13:19:51 +0900

    modify: 메인화면 예약 라이브  프리뷰 디자인 추가

--- front/farm-come-us/src/components/preview/LivePreviewList.js
+++ front/farm-come-us/src/components/preview/LivePreviewList.js
@@ -3,59 +3,16 @@ import React from "react";
 import classes from "./style/LivePreviewList.module.scss";
 
 import LiveItem from "./LiveItem";
+import ReservedLiveItem from "./ReservedLiveItem";
 
-// 더미 데이터
-const liveList = [
-  {
-    liveId: 1,
-    storeId: 1,
-    storeName: "강원 고랭 배추",
-    productId: 1,
-    productName: "강원도 고랭지 배추",
-    discount: 14,
-    price: 10800,
-    unit: 1,
-  },
-  {
-    liveId: 2,
-    storeId: 2,
-    storeName: "제주 당근당근",
-    productId: 2,
-    productName: "[서귀포] 신선 당근",
-    discount: 12,
-    price: 6200,
-    unit: 1,
-  },
-  {
-    liveId: 3,
-    storeId: 3,
-    storeName: "강원 고랭 배추",
-    productId: 3,
-    productName: "강원도 고랭지 배추",
-    discount: 14,
-    price: 10800,
-    unit: 3,
-  },
-  {
-    liveId: 4,
-    storeId: 4,
-    storeName: "제주 당근당근",
-    productId: 4,
-    productName: "[서귀포] 신선 당근",
-    discount: 16,
-    price: 13200,
-    unit: 1,
-  },
-];
+const LivePreviewList = (props) => {
+  const list = props.isLive
+    ? props.liveList.map((item) => <LiveItem key={item.liveId} live={item} />)
+    : props.liveList.map((item) => (
+        <ReservedLiveItem key={item.liveId} live={item} />
+      ));
 
-const LivePreviewList = () => {
-  return (
-    <ul className={`${classes.liveList}`}>
-      {liveList.map((item) => (
-        <LiveItem key={item.liveId} live={item} />
-      ))}
-    </ul>
-  );
+  return <ul className={`${classes.liveList}`}>{list}</ul>;
 };
 
 export default LivePreviewList;
