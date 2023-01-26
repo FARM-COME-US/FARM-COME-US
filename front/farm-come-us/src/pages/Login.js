585084f95385cc7cefbbc02f302e012ca27d7d49
CessnaJ <eon8748@gmail.com>
2023-01-26 11:12:57 +0900

    add: S08P12B103-266 userSlice, reduxStore

--- front/farm-come-us/src/pages/Login.js
+++ front/farm-come-us/src/pages/Login.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { asyncSomethingFetch } from "../reduxStore/userSlice";
+// 이 함수도 수정 필요 😀 기본형으로 해둠.
 
-const Login = () => {
-  return <div>로그인 화면입니다.</div>;
-};
+function Login() {
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <div>로그인 화면입니다.</div>
+
+      <button
+        onClick={() => {
+          dispatch(asyncSomethingFetch());
+        }}
+      />
+      {/* 로그인버튼. 아직 어떤 인자를 넣어서 비동기 요청 보낼지 안정함. userSlice도 수정필요 */}
+    </div>
+  );
+}
 
 export default Login;
--- front/farm-come-us/src/reduxStore/userSlice.js
+++ front/farm-come-us/src/reduxStore/userSlice.js
@@ -1,4 +1,5 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 // createAsyncThunk
 // import axios from "axios";
 
@@ -20,6 +21,17 @@ const initialStateValue = {
   nickname: "",
 }; //정확히 어떤 변수명으로 가지고 있어야할지 정하지 못함. BE와 문의
 
+const asyncSomethingFetch = createAsyncThunk(
+  "userSlice/something",
+  async () => {
+    const res = await axios.get("myServerURL");
+    const data = await res.json();
+    return data.value;
+    // value가 주어지는거에 따라서 pending, fulfilled, rejected가 달라짐.
+  }
+);
+// 비동기 처리하는 함수. 이름 안정함.
+
 const userSlice = createSlice({
   name: "user",
   initialState: {
@@ -36,7 +48,22 @@ const userSlice = createSlice({
     //   state.value = state.value + action.payload;
     // }, 참고용 예시 코드
   },
-  extraReducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(asyncSomethingFetch.pending, (state, action) => {
+      state.status = "loading";
+    });
+    // 요청 보내서 아직 못받은 상태
+    builder.addCase(asyncSomethingFetch.fulfilled, (state, action) => {
+      state.value = action.payload;
+      state.status = "complete";
+    });
+    // 요청 보내서 받은 상태. value도 업데이트해줌.
+    builder.addCase(asyncSomethingFetch.rejected, (state, action) => {
+      state.status = "fail";
+    });
+    // 요청 보내서 거절 상태. fail상태만 보여줌.
+  },
 });
 
+export { asyncSomethingFetch };
 export default userSlice;
