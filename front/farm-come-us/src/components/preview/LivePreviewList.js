bbd538e846194d69633491a6d73fece0b943e671
kevin-Arpe <arpeggiogio@gmail.com>
2023-01-27 01:49:21 +0900

    modify: 메인화면 상품 목록 디자인 구현

--- front/farm-come-us/src/components/preview/LivePreviewList.js
+++ front/farm-come-us/src/components/preview/LivePreviewList.js
@@ -6,11 +6,15 @@ import LiveItem from "./LiveItem";
 import ReservedLiveItem from "./ReservedLiveItem";
 
 const LivePreviewList = (props) => {
-  const list = props.isLive
-    ? props.liveList.map((item) => <LiveItem key={item.liveId} live={item} />)
-    : props.liveList.map((item) => (
-        <ReservedLiveItem key={item.liveId} live={item} />
-      ));
+  let list = <span>등록된 라이브가 없습니다.</span>;
+
+  if (props.liveList && props.liveList.length > 0) {
+    list = props.isLive
+      ? props.liveList.map((item) => <LiveItem key={item.liveId} live={item} />)
+      : props.liveList.map((item) => (
+          <ReservedLiveItem key={item.liveId} live={item} />
+        ));
+  }
 
   return <ul className={`${classes.liveList}`}>{list}</ul>;
 };
