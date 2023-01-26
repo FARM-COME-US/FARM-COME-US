7479bf4c2ac58100d661b4b366e6efb543e5da1e
euntol <mokbee27@gmail.com>
2023-01-26 17:47:38 +0900

    add: 상세목록페이지 구현 중

--- front/farm-come-us/src/pages/product/Products.js
+++ front/farm-come-us/src/pages/product/Products.js
@@ -1,5 +1,16 @@
+import React from "react";
+import Category from "../../components/product/Category.js";
+import RankSwitch from "../../components/product/RankSwitch.js";
+import ProductList from "../../components/product/ProductList.js";
+
 const Products = () => {
-return <div>asdasdasd</div>
+  return(
+    <div>
+      <Category />
+      <RankSwitch />
+      <ProductList />
+    </div>
+  );
 };
 
 
