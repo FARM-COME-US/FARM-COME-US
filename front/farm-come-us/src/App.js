4ece70abeaf124148017dca60ee2a285a014360d
kevin-Arpe <arpeggiogio@gmail.com>
2023-01-28 01:36:58 +0900

    modify: 라이브 목록 탭 컴포넌트 및 리스트 컴포넌트 추가

--- front/farm-come-us/src/App.js
+++ front/farm-come-us/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 import Header from "./components/common/Header";
 import Home from "./pages/Home";
@@ -8,17 +8,27 @@ import Login from "./pages/Login";
 import Signup from "./pages/SignUp";
 import Cart from "./pages/Cart";
 import Products from "./pages/product/Products";
+import Live from "./pages/live/Live";
+import RunningLive from "./pages/live/RunningLive";
+import ScheduledLive from "./pages/live/ScheduledLive";
+import NotFound from "./pages/NotFound";
 
 const App = () => {
   return (
     <div id="app">
       <Header />
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/login" element={<Login />}></Route>
-        <Route path="/sign-up" element={<Signup />}></Route>
-        <Route path="/cart" element={<Cart />}></Route>
-        <Route path="/products" element={<Products />}></Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/live" element={<Live />}>
+          <Route path="running" element={<RunningLive />} />
+          <Route path="scheduled" element={<ScheduledLive />} />
+          <Route path="" element={<Navigate replace to="running" />} />
+        </Route>
+        <Route path="/products" element={<Products />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
