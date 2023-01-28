b59b3f2a6e1ce19df6235b1f15c7649d30cef675
jieunlee0106 <jieun3181@gmail.com>
2023-01-28 22:44:41 +0900

    modify: cart

--- back/farmcu/src/main/java/com/ssafy/farmcu/entity/order/Order.java
+++ back/farmcu/src/main/java/com/ssafy/farmcu/entity/order/Order.java
@@ -4,6 +4,7 @@ import com.ssafy.farmcu.entity.member.Member;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
@@ -12,11 +13,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Getter
+@Setter
 @NoArgsConstructor
 @Entity
-@Table(name = "order_info")
-// 주문 그 자체, 예를 들어: order_info(pk = 1) = 배추*3 + 당근*4 의 값이 담겨 있음
-public class OrderInfo {
+@Table(name = "order")
+// 주문 그 자체, 예: order_info(pk = 1) = 배추*3 + 당근*4 의 값이 담겨 있음
+public class Order {
 
 //     주문 상태
 //     public emun OrderStatus{
@@ -35,6 +37,14 @@ public class OrderInfo {
     @CreationTimestamp
     private Timestamp orderCreateAt;
 
+    // 주문 - 결제 대기 - 결제 완료 - 배송 중 - 배송 완료 / B = before, A = after
+    public enum OrderStatus{
+        ORDER, BPAY, APAY, BSHIPPING, ASHIPPING
+    }
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+
     //연결
     @ManyToOne(cascade = CascadeType.MERGE, targetEntity = Member.class)
     @JoinColumn(name = "member_id", updatable = false)
@@ -45,12 +55,24 @@ public class OrderInfo {
     private DeliveryInfo delivery;
 
 
-    @OneToMany(mappedBy = "order")
-    private List<OrderItem> orderItem = new ArrayList<>();
+    @OneToMany(mappedBy = "order_info")
+    private List<OrderItem> orderItems = new ArrayList<>();
+
 
     @Builder
-    public OrderInfo(Timestamp orderCreateAt) {
+    public Order(Member member, Timestamp orderCreateAt, OrderStatus orderStatus, List<OrderItem> orderItems) {
+        this.member = member;
         this.orderCreateAt = orderCreateAt;
+        this.orderStatus = orderStatus;
+        this.orderItems = orderItems;
+    }
+
+    // 주문에 주문아이템 주입
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+        // OrderItem에 작성해둔 setOrder을 여기로 갖고 오는?
+
     }
 }
 
