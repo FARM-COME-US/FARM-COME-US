a0c8c9ac8b0fc5523cd5b640b391061e006da59e
kevin-Arpe <arpeggiogio@gmail.com>
2023-01-30 00:09:27 +0900

    modify: 마이페이지 live 목록 화면 구현

---
+++ front/farm-come-us/src/components/mystore/MyStoreLiveItem.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+import classes from "./style/MyStoreLiveItem.module.scss";
+
+const MyStoreLiveItem = (props) => {
+  const convertedPrice = props.item.price
+    .toString()
+    .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+  const reservedDate = () => {
+    const date = props.item.startDate;
+    const startTime = date.getTime();
+    const endDate = new Date(startTime + 1 * 60 * 60 * 1000);
+
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const dt = date.getDate();
+    let hour = date.getHours();
+    hour = hour < 12 ? "0" + hour : hour;
+
+    let minute = date.getMinutes();
+    minute = minute < 10 ? "0" + minute : minute;
+
+    const endYear = endDate.getFullYear();
+    const endMonth = endDate.getMonth() + 1;
+    const endDt = endDate.getDate();
+    let endHour = endDate.getHours();
+    endHour = endHour < 12 ? "0" + endHour : endHour;
+
+    let endMinute = endDate.getMinutes();
+    endMinute = endMinute < 10 ? "0" + endMinute : endMinute;
+
+    return `${year}.${month}.${dt} ${hour}:${minute} ~ ${endYear}.${endMonth}.${endDt} ${endHour}:${endMinute}`;
+  };
+
+  return (
+    <div className={classes.liveItem}>
+      <img src={props.item.imgSrc} alt="live_img" />
+      <div className={classes.liveInfo}>
+        <div>
+          <span>{props.item.liveTitle}</span>
+          <span>{`${props.item.stock}상자`}</span>
+        </div>
+        <div>{`${convertedPrice}/${props.item.unit}상자`}</div>
+        <div>{`${reservedDate()}`}</div>
+      </div>
+    </div>
+  );
+};
+
+export default MyStoreLiveItem;
--- front/farm-come-us/src/pages/mystore/MyStoreLive.js
+++ front/farm-come-us/src/pages/mystore/MyStoreLive.js
@@ -1,11 +1,38 @@
 import React from "react";
 
+import classes from "./style/MyStoreLive.module.scss";
+
 import MyStoreContentTitle from "../../components/mystore/MyStoreContentTItle";
+import MyStoreLiveList from "../../components/mystore/MyStoreLiveList";
+import AddButton from "../../components/mystore/AddButton";
+
+const DUMMY_LIVE_LIST = [
+  {
+    liveId: 1,
+    liveTitle: "신선한 유기농 강원 고랭 배추",
+    productId: 1,
+    productName: "강원도 고랭지 배추",
+    stock: 140,
+    price: 14000,
+    unit: 1,
+    startDate: new Date(2022, 1, 10, 23, 0, 0),
+    imgSrc: "https://via.placeholder.com/300",
+  },
+];
 
 const MyStoreLive = () => {
+  const addLiveHandler = () => {
+    alert("라이브 추가 이벤트 발생");
+    return;
+  };
+
   return (
-    <div>
+    <div className={classes.liveContainer}>
       <MyStoreContentTitle text="Live" />
+      <MyStoreLiveList lives={DUMMY_LIVE_LIST} />
+      <div className={classes.btnBox}>
+        <AddButton className={classes.btnAdd} onClick={addLiveHandler} />
+      </div>
     </div>
   );
 };
