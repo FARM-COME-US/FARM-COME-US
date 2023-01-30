525a97f9697629f627df8995963d6d1d44d4f67c
lab.ssafy <jieun3181@gmail.com>
2023-01-30 20:42:48 +0900

    add: ItemNotFoundException 파일추가

--- back/farmcu/src/main/java/com/ssafy/farmcu/dto/order/RequestCartDto.java
+++ back/farmcu/src/main/java/com/ssafy/farmcu/dto/order/RequestCartDto.java
@@ -3,22 +3,21 @@ package com.ssafy.farmcu.dto.order;
 import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 import java.security.Timestamp;
 
+@Getter @Setter
+@AllArgsConstructor
+@ToString
 public class RequestCartDto {
-    @Getter
-    @AllArgsConstructor
-    @ToString
-    public static class Post {
-        @NotNull
-        private Long member_id;
-        @NotNull
-        private Long item_id;
-        @NotNull
-        private Integer cartItemCount;
-    }
 
+    @NotNull
+    private Long memberId;
+    @NotNull
+    private Long itemId;
+    @NotNull
+    private Integer cartItemCount;
 
 }
--- back/farmcu/src/main/java/com/ssafy/farmcu/repository/ItemRepository.java
+++ back/farmcu/src/main/java/com/ssafy/farmcu/repository/ItemRepository.java
@@ -1,7 +1,14 @@
 package com.ssafy.farmcu.repository;
 
+import com.ssafy.farmcu.entity.store.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface ItemRepository {
+public interface ItemRepository extends JpaRepository<Item, Long> {
+    Optional<Item> findById(Long num);
+
+
 }
