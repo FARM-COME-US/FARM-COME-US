96992762da19f1b2219ca21264d47b336fb822ed
Euntol <mokbee27@gmail.com>
2023-01-31 06:27:44 +0900

    add: cart apge

--- front/farm-come-us/src/pages/Cart.js
+++ front/farm-come-us/src/pages/Cart.js
@@ -20,7 +20,25 @@ const Cart = () => {
             <div className={classes.storename}>title</div>
             <div className={classes.productcount}>상품 n개</div>
           </div>
-          <div className={classes.cardbody}></div>
+          <div className={classes.cardbody}>
+            <input type="checkbox" />
+            <div className={classes.imgline}>
+              <div className={classes.imgbox}></div>
+            </div>
+            <div className={classes.script}>
+              <div className={classes.firstline}>
+                <div className={classes.productname}>상품 이름</div>
+                <div className={classes.poductweight}>무게</div>
+              </div>
+              <div className={classes.secondline}>
+                <div className={classes.discountrate}>할인률</div>
+                <div className={classes.originalprice}>정가</div>
+              </div>
+              <div className={classes.thirdline}>
+                <div className={classes.discountprice}>할인가</div>
+              </div>
+            </div>
+          </div>
           <div className={classes.cardfoot}>
             <div className={classes.shippingfee}>배송비 2500원</div>
             <div className={classes.secondline}>
