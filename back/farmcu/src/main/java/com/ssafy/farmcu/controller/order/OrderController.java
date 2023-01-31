b70b5a2b163645ca67f6ccb842f80c6b640e2b52
JieunLee <jieun3181@gmail.com>
2023-01-31 12:33:29 +0900

    modify: controller/order

---
+++ back/farmcu/src/main/java/com/ssafy/farmcu/controller/order/OrderController.java
@@ -0,0 +1,13 @@
+package com.ssafy.farmcu.controller.order;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("/order")
+public class OrderController {
+
+}
