5296de707c5ada8bf77aa0cef9eacee9372c145d
CessnaJ <eon8748@gmail.com>
2023-02-01 02:09:55 +0900

    chore : 코드정리

--- front/farm-come-us/src/reduxStore/store.js
+++ front/farm-come-us/src/reduxStore/store.js
@@ -1,7 +1,4 @@
 import { configureStore } from "@reduxjs/toolkit";
-import storage from "redux-persist/lib/storage/session";
-import { combineReducers } from "@reduxjs/toolkit";
-import { persistReducer } from "redux-persist";
 import userSlice from "./userSlice";
 import menuSlice from "./menuSlice";
 
