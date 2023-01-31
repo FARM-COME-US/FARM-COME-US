95bc52a19b414bc2fe4fc12b6b37ba395018575b
JieunLee <jieun3181@gmail.com>
2023-01-31 12:33:29 +0900

    modify: controller/order

--- back/farmcu/src/main/java/com/ssafy/farmcu/controller/order/CartController.java
+++ back/farmcu/src/main/java/com/ssafy/farmcu/controller/order/CartController.java
@@ -2,9 +2,11 @@ package com.ssafy.farmcu.controller.order;
 
 
 import com.ssafy.farmcu.service.order.CartService;
+import io.swagger.models.Model;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequiredArgsConstructor
@@ -21,6 +23,18 @@ public class CartController {
     //- select ex) selectMember (조회)
     //- 7update ex) updateMemberInfo (업데이트)
     //- Delete ex) deleteMember (삭제)
+
+    //** 주문 목록 조회 **//
+//    @GetMapping("/list/{meberId}")
+//    public String selectCart(Model model) {
+//
+//    }
+//
+//    //** 주문 상세 조회 **//
+//    public String selectC
+
+    //** 주문 완료 **//
+
 
 
 }
