f468cefbe01e714680a6348a3af340de3fd3f480
4priltwntsx <4priltwntsx@naver.com>
2023-02-01 03:21:36 +0900

    add: 소셜로그인 Controller, 일반 로그인 Controller 추가

--- back/farmcu/src/main/java/com/ssafy/farmcu/service/member/MemberService.java
+++ back/farmcu/src/main/java/com/ssafy/farmcu/service/member/MemberService.java
@@ -1,9 +1,6 @@
 package com.ssafy.farmcu.service.member;
 
-import com.ssafy.farmcu.dto.member.MemberJoinReq;
-import com.ssafy.farmcu.dto.member.MemberLoginReq;
-import com.ssafy.farmcu.dto.member.MemberUpdateReq;
-import com.ssafy.farmcu.dto.member.MemberInfoRes;
+import com.ssafy.farmcu.dto.member.*;
 import com.ssafy.farmcu.entity.member.Member;
 
 public interface MemberService {
@@ -11,8 +8,9 @@ public interface MemberService {
     // 회원 가입
     public boolean createMember(MemberJoinReq memberJoinInfo);
     // 회원 조회
-    public Member getMemberById(String Id);
-    public MemberInfoRes getMemberPhoto(String Id);
+    public MemberResponseDto getUserInfo(String Id);
+
+    //    public MemberInfoRes getMemberPhoto(String Id);
     // 회원 삭제
     public boolean deleteMember(MemberLoginReq memberLoginReq);
 
