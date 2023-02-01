97ea6a4cec0a8ea16f31056e04e67c4db98eed5d
CessnaJ <eon8748@gmail.com>
2023-02-01 17:44:02 +0900

    chore : 회원관련 api 수정 중

--- front/farm-come-us/src/pages/Login.js
+++ front/farm-come-us/src/pages/Login.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { MdPermIdentity } from "react-icons/md";
+import { MdPermIdentity, MdLockOutline } from "react-icons/md";
 import jwt_decode from "jwt-decode";
 import axios from "axios";
 
