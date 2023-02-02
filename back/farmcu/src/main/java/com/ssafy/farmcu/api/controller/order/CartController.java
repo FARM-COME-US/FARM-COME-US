8a0f9a25d5f1ed6998d514ee0d60edbad7441bf6
JieunLee <jieun3181@gmail.com>
2023-02-02 17:46:31 +0900

    modify: 오류 수정 중

--- back/farmcu/src/main/java/com/ssafy/farmcu/api/controller/order/CartController.java
+++ back/farmcu/src/main/java/com/ssafy/farmcu/api/controller/order/CartController.java
@@ -1,11 +1,12 @@
 package com.ssafy.farmcu.api.controller.order;
 
 
+import com.ssafy.farmcu.api.dto.item.ItemDto;
 import com.ssafy.farmcu.api.dto.order.CartOrderDto;
 import com.ssafy.farmcu.api.dto.order.CartRequestDto;
 import com.ssafy.farmcu.api.entity.member.Member;
 import com.ssafy.farmcu.api.entity.order.Cart;
-import com.ssafy.farmcu.api.service.order.CartServiceImpl;
+import com.ssafy.farmcu.api.service.order.CartService;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,7 +26,7 @@ import java.util.List;
 @RequestMapping("/cart")
 public class CartController {
     @Autowired
-    private final CartServiceImpl cartServiceImpl;
+    private final CartService cartService;
 
     // Controller method
     //
@@ -37,58 +38,58 @@ public class CartController {
 
     //** 로그인 한 사용자의 장바구니 목록 조회 **//
     @GetMapping("")
-    @ApiOperation(value="회원 탈퇴", notes="")
+    @ApiOperation(value=" 사용자 장바구니 조회")
     public String selectMyCart(Model model) {
         Member member = (Member) SecurityContextHolder.getContext().getAuthentication().getPrincipal(); //현재 로그인 정보
-        List<Cart> cart = cartServiceImpl.findMyCart(member); //멤버의 주문목록 불러오기
-        model.addAttribute("cart", cart); //멤버의 주문리스트 뷰로 전송
+        List<Cart> cart = cartService.findMyCart(member); //멤버의 주문목록 불러오기
 
-        model.addAttribute("cart", cart); //멤버의 주문리스트 뷰로 전송
-        return "/member/myCart";
+            return "modify";
+//        return ResponseEntity.ok(CartService.findMyCart(member));
     }
 
-
-    @PostMapping(value = "") //주문하기,
-    public ResponseEntity createCart(CartRequestDto cartDto, BindingResult bindingResult, Principal principal) {
-        if (bindingResult.hasErrors()) {
-            StringBuilder sb = new StringBuilder();
-            List<FieldError> fieldErrors = bindingResult.getFieldErrors();
-            for (FieldError fieldError : fieldErrors) {
-                sb.append(fieldError.getDefaultMessage());
-            }
-            return new ResponseEntity<String>(sb.toString(), HttpStatus.BAD_REQUEST);
-        }
-
-        String name = principal.getName(); //현재 로그인 정보에서 이름 가져오기
-        Long cartId; //장바구니번호 생성
-
-        try {
-            cartId = cartServiceImpl.addCart(cartDto, name); //주문 시도 및 주문번호 가져오기
-        } catch (Exception e) {
-            return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
-        }
-
-        return new ResponseEntity<Long>(cartId, HttpStatus.OK); //장바구니번호 리턴
-    }
+//    @PostMapping(value = "") //주문하기,
+//    public ResponseEntity createCart(CartRequestDto cartDto, BindingResult bindingResult, Principal principal) {
+//        if (bindingResult.hasErrors()) {
+//            StringBuilder sb = new StringBuilder();
+//            List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+//            for (FieldError fieldError : fieldErrors) {
+//                sb.append(fieldError.getDefaultMessage());
+//            }
+//            return new ResponseEntity<String>(sb.toString(), HttpStatus.BAD_REQUEST);
+//        }
+//
+//        String name = principal.getName(); //현재 로그인 정보에서 이름 가져오기
+//        Long cartId; //장바구니번호 생성
+//
+//        try {
+//            cartId = cartService.addCart(cartDto, name); //주문 시도 및 주문번호 가져오기
+//        } catch (Exception e) {
+//            return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+//        }
+//
+//        return new ResponseEntity<Long>(cartId, HttpStatus.OK); //장바구니번호 리턴
+//
+//    }
 
     //** 장바구니 상품 주문 **//                  <======== 상품 바로 주문은 OrderController
-    @PostMapping(value = "")
-    public ResponseEntity cartOrder(CartOrderDto cartOrderDto, Principal principal) {
-        List<CartOrderDto> cartOrderDtoList = cartOrderDto.getCartOrderDtoList(); //전달된 장바구니의 항목 리스트
-
-        if (cartOrderDtoList == null || cartOrderDtoList.size() == 0) { //리스트가 비었거나 0개면
-            return new ResponseEntity<String>("선택된 상품이 없습니다.", HttpStatus.BAD_REQUEST);
-        }
-        //주문로직에 장바구니 리스트와 멤버 정보 전달
-        Long orderId = cartServiceImpl.orderCart(cartOrderDtoList, principal.getName());
-        //주문번호 리턴
-        return new ResponseEntity<Long>(orderId, HttpStatus.OK);
-    }
+//    @PostMapping(value = "")
+//    public ResponseEntity cartOrder(CartOrderDto cartOrderDto, Principal principal) {
+//        List<CartOrderDto> cartOrderDtoList = cartOrderDto.getCartOrderDtoList(); //전달된 장바구니의 항목 리스트
+//
+//        if (cartOrderDtoList == null || cartOrderDtoList.size() == 0) { //리스트가 비었거나 0개면
+//            return new ResponseEntity<String>("선택된 상품이 없습니다.", HttpStatus.BAD_REQUEST);
+//        }
+//        //주문로직에 장바구니 리스트와 멤버 정보 전달
+//        Long orderId = cartService.orderCart(cartOrderDtoList, principal.getName());
+//        //주문번호 리턴
+//        return new ResponseEntity<Long>(orderId, HttpStatus.OK);
+//
+//    }
 
     @DeleteMapping(value = "/deleteCart") //장바구니 삭제
     public String deleteCart(Long id) {
-        cartServiceImpl.deleteById(id);
-        return "redirect:/myCart";
+        cartService.deleteById(id);
+        return "modify";
 //        return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 
--- back/farmcu/src/main/java/com/ssafy/farmcu/api/controller/order/OrderController.java
+++ back/farmcu/src/main/java/com/ssafy/farmcu/api/controller/order/OrderController.java
@@ -3,7 +3,7 @@ package com.ssafy.farmcu.api.controller.order;
 import com.ssafy.farmcu.api.dto.order.OrderDto;
 import com.ssafy.farmcu.api.entity.member.Member;
 import com.ssafy.farmcu.api.entity.order.OrderItem;
-import com.ssafy.farmcu.api.service.order.OrderServiceImpl;
+import com.ssafy.farmcu.api.service.order.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -23,12 +23,12 @@ import java.util.List;
 public class OrderController {
 
     @Autowired
-    private final OrderServiceImpl orderServiceImpl;
+    private final OrderService orderService;
 
     //** 전체 주문 목록 조회 **//
     @GetMapping("") //전체 주문 목록 확인
     public String selectOrders(Model model){
-        List<OrderItem> orders = orderServiceImpl.findAllItems(); //모든 주문 불러오기
+        List<OrderItem> orders = orderService.findAllItems(); //모든 주문 불러오기
 
         model.addAttribute("orders", orders); //주문 리스트를 뷰로 전송
         return "/admin/orderList";
@@ -38,7 +38,7 @@ public class OrderController {
     @GetMapping("")
     public String selectMyOrders(Model model){
         Member member = (Member) SecurityContextHolder.getContext().getAuthentication().getPrincipal(); //현재 로그인 정보
-        List<OrderItem> orders = orderServiceImpl.findMyItems(member); //멤버의 주문목록 불러오기
+        List<OrderItem> orders = orderService.findMyItems(member); //멤버의 주문목록 불러오기
 
         model.addAttribute("orders", orders); //멤버의 주문리스트 뷰로 전송
         return "/order/myOrder";
@@ -64,7 +64,7 @@ public class OrderController {
         Long orderId; //주문번호 생성
 
         try {
-            orderId = orderServiceImpl.order(orderDto, name); //주문 시도 및 주문번호 가져오기
+            orderId = orderService.order(orderDto, name); //주문 시도 및 주문번호 가져오기
         } catch (Exception e) {
             return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
@@ -76,7 +76,7 @@ public class OrderController {
     //** 주문 취소 **//
     @PutMapping
     public String updateOrder(Long orderId){
-        orderServiceImpl.updateOrder(orderId);
+        orderService.updateOrder(orderId);
 
         return "redirect:/myOrders";
     }
