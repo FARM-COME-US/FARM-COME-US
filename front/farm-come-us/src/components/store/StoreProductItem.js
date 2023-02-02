57d5f9fa1a0ff95488713c4fbc258fe6fa2f38e4
euntol <mokbee27@gmail.com>
2023-02-02 13:13:16 +0900

    add: 방문자 스토어 페이지

--- front/farm-come-us/src/components/store/StoreProductItem.js
+++ front/farm-come-us/src/components/store/StoreProductItem.js
@@ -1,9 +1,28 @@
 import React from "react";
+import classes from "./style/StoreProductItem.module.scss";
 
-const StoreProductItem = () => {
-  return(
-    <div></div>
-  )
-}
+const StoreProductItem = (props) => {
+  const convertedPrice = props.product.productPrice
+    .toString()
+    .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+  return (
+    <div className={classes.container}>
+      <div className={classes.imagespace}></div>
+      <div className={classes.scriptspace}>
+        <div className={classes.firstline}>
+          <div className={classes.productname}>{props.product.productName}</div>
+          <div className={classes.productamount}>
+            {props.product.productAmount}kg
+          </div>
+        </div>
+        <div className={classes.thirdline}>{props.product.productScript}</div>
+        <div className={classes.secondline}>
+          {props.product.productOption}kg / {convertedPrice}원
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default StoreProductItem;
