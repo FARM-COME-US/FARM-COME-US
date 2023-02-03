672c3958e10530727bdc5f2d80af2f34d9364c86
CessnaJ <eon8748@gmail.com>
2023-02-03 17:50:52 +0900

    add : 마이스토어 - 관심스토어, 구매내역

--- front/farm-come-us/src/pages/mypage/MyReceipts.js
+++ front/farm-come-us/src/pages/mypage/MyReceipts.js
@@ -3,7 +3,7 @@ import MyReceiptItem from "../../components/mypage/MyReceiptItem";
 import classes from "./style/MyReceipts.module.scss";
 
 const MyReceipts = (props) => {
-  const [myReceiptsInfoArr, setReceiptsInfoArr] = useState("");
+  const [myReceiptsInfoArr, setReceiptsInfoArr] = useState("1");
   // receiptsInfoArr가 list형태의 객체들로 들어올것이고, [obj1, obj2... 이렇게.]
   // item1 = { id:orderId, items:[강원도배추, 제즈스윗당근], cost:429,000, orderdate:2023.01.10 21:12:58, }
 
@@ -17,22 +17,26 @@ const MyReceipts = (props) => {
 
   let list = <span className={classes.noItem}>구매 내역이 없습니다.</span>;
 
-  if (myReceiptsInfoArr.length > 0) {
-    list = props.myReceipts.map((item) => (
-      <MyReceiptItem key={item.id} info={item} />
-    ));
-  }
+  // if (myReceiptsInfoArr.length > 0) {
+  //   list = props.myReceipts.map((item) => (
+  //     <MyReceiptItem key={item.id} info={item} />
+  //   ));
+  // }
+
+  // list = <MyReceiptItem />;
 
   return (
     <div>
       <div className={classes.header}>구매내역</div>
 
       <div
-        className={`${classes.flexbox} ${
+        className={`${classes.flexbox} ${classes.mt} ${
           myReceiptsInfoArr ? "" : classes.centerAlignWrapper
         }`}
       >
-        {list}
+        <MyReceiptItem />
+        <MyReceiptItem />
+        <MyReceiptItem />;{/* {list} 원상복구 필요. 수정필요*/}
       </div>
     </div>
   );
