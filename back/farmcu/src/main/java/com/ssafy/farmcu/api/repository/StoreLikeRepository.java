1f1afbb3c31fc83f2d7c31f17205f1b72106e807
김지희 <cjg05034@naver.com>
2023-02-05 02:49:00 +0900

    modify: 스토어 찜하기/찜취소 에러 수정

--- back/farmcu/src/main/java/com/ssafy/farmcu/api/repository/StoreLikeRepository.java
+++ back/farmcu/src/main/java/com/ssafy/farmcu/api/repository/StoreLikeRepository.java
@@ -1,5 +1,6 @@
 package com.ssafy.farmcu.api.repository;
 
+import com.ssafy.farmcu.api.dto.member.MemberListRes;
 import com.ssafy.farmcu.api.entity.member.Member;
 import com.ssafy.farmcu.api.entity.store.Store;
 import com.ssafy.farmcu.api.entity.store.StoreLike;
@@ -12,10 +13,14 @@ import java.util.Optional;
 public interface StoreLikeRepository extends JpaRepository<StoreLike, Long> {
 
     @Query(value = "SELECT s FROM StoreLike s WHERE s.member.memberId = :memberId")
-    Optional<List<StoreLike>> findStoreLikeByMember(Long memberId);
+    List<StoreLike> findStoreLikeByMember(Long memberId);
 
-    @Query(value = "SELECT s.member.memberId FROM StoreLike s WHERE s.store.storeId = :storeId")
-    Optional<List<String>> findStoreLikeByStore(Long storeId);
+//    @Query(value = "SELECT s.member FROM StoreLike s WHERE s.store.storeId = :storeId")
+//    List<Member> findStoreLikeByStore(Long storeId);
 
+    @Query("select s.member.memberId from StoreLike s where s.store.storeId = :storeId")
+    List<Long> findMemberIdByStoreId(Long storeId);
 
+    @Query("select new com.ssafy.farmcu.api.dto.member.MemberListRes(m.memberId, m.id, m.name, m.nickname, m.profileImg) from Member m join StoreLike sl where sl.store.storeId = :storeId")
+    List<MemberListRes> findStoreLikeByStore(Long storeId);
 }
--- back/farmcu/src/main/java/com/ssafy/farmcu/api/service/store/StoreLikeService.java
+++ back/farmcu/src/main/java/com/ssafy/farmcu/api/service/store/StoreLikeService.java
@@ -1,5 +1,7 @@
 package com.ssafy.farmcu.api.service.store;
 
+import com.ssafy.farmcu.api.dto.member.MemberListRes;
+import com.ssafy.farmcu.api.dto.store.StoreLikeCreateDto;
 import com.ssafy.farmcu.api.dto.store.StoreLikeDto;
 import com.ssafy.farmcu.api.dto.store.StoreLikeStoreListDto;
 
@@ -13,10 +15,12 @@ import java.util.List;
  * - update
  */
 public interface StoreLikeService {
-    public boolean saveLike(StoreLikeDto storeLikeDto);
+    public boolean saveLike(StoreLikeCreateDto storeLikeDto);
     public boolean deleteLike(StoreLikeDto storeLikeDto);
     public List<StoreLikeStoreListDto> findLikes(Long memberId); // 멤버가 좋아요한 스토어 목록
-    public List<String> findLikesId(Long storeId); // 해당 스토어의 좋아요 누른 유저 리스트
+    public List<Long> findLikesId(Long storeId); // 해당 스토어의 좋아요 누른 유저 리스트
+    public List<MemberListRes> findLikesMembers(Long storeId); // 해당 스토어의 좋아요 누른 유저 리스트
+
     public int getCount(Long storeId); // 해당 스토어 좋아요 누른 유저 count
 
 }
