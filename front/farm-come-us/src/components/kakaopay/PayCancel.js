61f92fbd698de8bfa66103967a9059f06fe473cf
CessnaJ <eon8748@gmail.com>
2023-02-06 01:31:39 +0900

    feat : 카카오 결제 취소시, 메세지 보여주고 홈으로 가는 기능 추가

--- front/farm-come-us/src/components/kakaopay/PayCancel.js
+++ front/farm-come-us/src/components/kakaopay/PayCancel.js
@@ -1,3 +1,12 @@
-const PayCancel = (props) => {};
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+const PayCancel = (props) => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    setTimeout(navigate("/"), 2000);
+  }, []);
+  return <div>결제 취소</div>;
+};
 
 export default PayCancel;
