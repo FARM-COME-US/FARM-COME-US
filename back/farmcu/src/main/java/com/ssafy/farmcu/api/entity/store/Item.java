2b506eca04b93bf9b9ee1d94e3370ab526fad8ff
chahk03 <chku52@gmail.com>
2023-02-06 15:02:36 +0900

    feat: 상품 카테고리 분류

--- back/farmcu/src/main/java/com/ssafy/farmcu/api/entity/store/Item.java
+++ back/farmcu/src/main/java/com/ssafy/farmcu/api/entity/store/Item.java
@@ -45,28 +45,17 @@ public class Item {
     private Timestamp itemCreatedAt;
 
     //연결
-    @ManyToOne(cascade = CascadeType.MERGE, targetEntity = Category.class)
+    @ManyToOne(cascade = CascadeType.MERGE, targetEntity = CategoryDetail.class)
     @JoinColumn(name = "category_code", updatable = false)
-    private Category category;
+    private CategoryDetail categoryDetail;
 
     @ManyToOne(cascade = CascadeType.MERGE, targetEntity = Store.class)
     @JoinColumn(name = "store_id", updatable = false)
     private Store store;
 
-    //연관 관계 메서드
-//    public void setCategory(Category category) {
-//        this.category = category;
-//        category.getItem().add(this);
-//    }
-//
-//    public void setStore(Store store) {
-//        this.store = store;
-//        store.getItem().add(this);
-//    }
-
     //빌더
     @Builder
-    public Item(Long itemId, String itemName, String itemDescription, int itemDiscount, int itemPrice, int itemStock, Timestamp itemCreatedAt, Category category, Store store ) {
+    public Item(Long itemId, String itemName, String itemDescription, int itemDiscount, int itemPrice, int itemStock, Timestamp itemCreatedAt, CategoryDetail categoryDetail, Store store ) {
         this.itemId = itemId;
         this.itemName = itemName;
         this.itemDescription = itemDescription;
@@ -74,7 +63,7 @@ public class Item {
         this.itemDiscount = itemDiscount;
         this.itemStock = itemStock;
         this.itemCreatedAt = itemCreatedAt;
-        this.category = category;
+        this.categoryDetail = categoryDetail;
         this.store = store;
     }
 
