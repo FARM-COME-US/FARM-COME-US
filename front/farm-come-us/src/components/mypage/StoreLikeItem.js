b1791da59701e8edf404c12237cf53fc162e0b22
CessnaJ <eon8748@gmail.com>
2023-02-07 13:51:58 +0900

    feat : 찜 해제기능 추가, 관심스토어 및 구매내역 레이아웃수정

--- front/farm-come-us/src/components/mypage/StoreLikeItem.js
+++ front/farm-come-us/src/components/mypage/StoreLikeItem.js
@@ -1,33 +1,66 @@
+import { useState, useEffect } from "react";
 import classes from "./style/StoreLikeItem.module.scss";
-import { AiFillHeart } from "react-icons/ai";
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+import axios from "axios";
 
 const StoreLikeItem = (props) => {
+  const [isLike, setIsLike] = useState(true);
+  // const data = {
+  //   img_address: "/img/cabbage.png",
+  //   isLike: true,
+  //   title: "고랭강원농장",
+  //   address: "강원도 평창군 봉평면 무이리 23-12",
+  //   representative: "강남자",
+  //   created: "2020. 05. 10",
+  // };
+
+  useEffect(() => {
+    return () => {
+      if (isLike !== props.isLike) {
+        axios.post(
+          `"지워졌다는걸 알려주는 delete함수. 유저정보, 스토어정보. 지워졌다!"
+          "userid"
+          "token"
+          "storeid"}`
+        );
+      }
+    };
+  }, []);
+
   return (
     <div className={`${classes.card} ${classes.mt}`}>
       <div className={`${classes.imgWrapper}`}>
         <img
           className={classes.img}
-          src={process.env.PUBLIC_URL + "/img/cabbage.png"}
+          src={`${process.env.PUBLIC_URL}${props.img_address}`}
         />
       </div>
       <div className={`${classes.colflexbox} ${classes.mt}`}>
-        <div className={classes.heart}>
-          <AiFillHeart />
+        <div
+          className={classes.heartsize}
+          onClick={() => {
+            console.log(props.title);
+            setIsLike(!isLike);
+          }}
+        >
+          {isLike ? (
+            <AiFillHeart className={classes.filledheart} />
+          ) : (
+            <AiOutlineHeart className={classes.emptyheart} />
+          )}
         </div>
-        <div className={classes.title}>고랭강원농장</div>
+        <div className={classes.title}>{props.title}</div>
 
         <div className={classes.rowflexbox}>
           <div
             className={`${classes.description} ${classes.fullwidth} `}
-          >{`주소${" : "} `}</div>
-          <div className={classes.description}>
-            강원도 평창군 봉평면 무이리 23-12
-          </div>
+          >{`주소${" : "}`}</div>
+          <div className={classes.description}>{props.address}</div>
         </div>
-        <div className={classes.description}>{`대표자 : ${"강남자"}`}</div>
         <div
           className={classes.description}
-        >{`등록일 : ${"2020. 05. 10"}`}</div>
+        >{`대표자 : ${props.representative}`}</div>
+        <div className={classes.description}>{`등록일 : ${props.created}`}</div>
       </div>
     </div>
   );
