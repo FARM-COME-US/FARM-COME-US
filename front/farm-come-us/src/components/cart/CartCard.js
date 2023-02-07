7d37e23273f7251e0c96c1aad4fdceb8c40ef142
euntol <mokbee27@gmail.com>
2023-02-07 13:54:21 +0900

    modify: cart

--- front/farm-come-us/src/components/cart/CartCard.js
+++ front/farm-come-us/src/components/cart/CartCard.js
@@ -8,15 +8,30 @@ const CartCard = (props) => {
     <CartItem key={item.productId} item={item}></CartItem>
   ));
 
+  const shippingFee = 2500;
+
   return (
     <div className={classes.container}>
       <Card className={classes.card}>
         <div className={classes.cardHeader}>
-          <div className={classes.storeName}>{props.itemList[0].storeName}</div>
-          <div>상품 n개</div>
+          <div className={classes.buttonSpace}>
+            <div className={classes.button}></div>
+          </div>
+          <div className={classes.textArea}>
+            <div className={classes.storeName}>
+              {props.itemList[0].storeName}
+            </div>
+            <div className={classes.productAmount}>상품 n개</div>
+          </div>
         </div>
         <div className={classes.cardBody}>{item}</div>
-        <div className={classes.cardFooter}></div>
+        <div className={classes.cardFooter}>
+          <div className={classes.shippingFee}>배송비 {shippingFee}원</div>
+          <div className={classes.resultPrice}>
+            <div className={classes.text}>총 금액</div>
+            <div className={classes.price}>원</div>
+          </div>
+        </div>
       </Card>
     </div>
   );
--- front/farm-come-us/src/components/cart/CartItem.js
+++ front/farm-come-us/src/components/cart/CartItem.js
@@ -1,15 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./style/CartItem.module.scss";
-import Card from "../common/Card";
 
 const CartItem = (props) => {
+  const [productIdList, setList] = useState([]);
+
+  const appendSetList = () => {
+    setList([...productIdList, props.item.productId]);
+    console.log(productIdList);
+  };
+
+  const removeSetList = (id) => {
+    setList(productIdList.filter((id) => productIdList !== id));
+  };
+
   return (
     <div className={classes.container}>
-      <Card className={classes.card}>
-        <div className={classes.cardHeader}></div>
-        <div className={classes.cardBody}></div>
-        <div className={classes.cardFooter}></div>
-      </Card>
+      <div className={classes.buttonSpace}>
+        <div className={classes.button} onClick={appendSetList}></div>
+      </div>
+      <div className={classes.imgSpace}>
+        <img src="https://via.placeholder.com/300" alt="productImg" />
+      </div>
+      <div className={classes.itemDes}>
+        <div className={classes.topSpace}>
+          <div>{props.item.productName}</div>
+        </div>
+        <div className={classes.middleSpace}>
+          <div className={classes.option}>
+            옵션: {props.item.productOption}개
+          </div>
+          <div className={classes.secondSpace}>
+            <div className={classes.discount}>{props.item.discount}%</div>
+            <div className={classes.price}>{props.item.price}</div>
+          </div>
+        </div>
+        <div className={classes.bottomSpace}>
+          <div className={classes.discountPrice}>
+            {props.item.discountPrice}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
