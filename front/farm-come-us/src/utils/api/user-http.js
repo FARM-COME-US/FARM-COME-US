c1da8f4bf84ede0df76d6abfba7d14ed887b0413
kevin-Arpe <arpeggiogio@gmail.com>
2023-02-07 18:00:23 +0900

    modify: Rest API 로컬 테스트

--- front/farm-come-us/src/utils/api/user-http.js
+++ front/farm-come-us/src/utils/api/user-http.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const DUMMY_SERVER_URL = "https:localhost:9090";
-const USER_API_URL = `${DUMMY_SERVER_URL}`;
+const DUMMY_SERVER_URL = "http://localhost:9090";
+const USER_API_URL = `${DUMMY_SERVER_URL}/member`;
 
 const clientOrderObjFormatter = (obj) => {
   return {};
@@ -10,6 +10,7 @@ const clientOrderObjFormatter = (obj) => {
 const serverOrderObjFormatter = (obj) => {
   return {
     id: obj.userId,
+    password: obj.password,
     nickname: obj.nickname,
     name: obj.name,
     email: obj.email,
@@ -21,13 +22,35 @@ const serverOrderObjFormatter = (obj) => {
 };
 
 export async function userSignUp(userInfo) {
+  console.log(userInfo);
+  let data = serverOrderObjFormatter(userInfo);
+  data = {
+    ...data,
+    id: data.userId,
+  };
+  data["id"] = userInfo.userId;
+  console.log(data);
   try {
-    const response = axios({
-      method: "post",
-      url: `${USER_API_URL}/join`,
-      data: {
-        request: serverOrderObjFormatter(userInfo),
+    const response = axios.post("api/member/join", {
+      data,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
       },
+      withCredentials: false,
+    });
+
+    console.log(response);
+  } catch (err) {
+    console.err(err);
+  }
+}
+
+export async function login(id, password) {
+  try {
+    const response = axios.post(`${USER_API_URL}/login`, {
+      id: id,
+      password: password,
     });
     console.log(response);
   } catch (err) {
