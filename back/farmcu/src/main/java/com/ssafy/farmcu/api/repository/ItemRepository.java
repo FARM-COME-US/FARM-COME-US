3adf88ce8db6efafba013ce77a59e982bfa10093
차현경 <h30young@naver.com>
2023-02-08 17:52:29 +0900

    modify: 아이템 이미지 저장

--- back/farmcu/src/main/java/com/ssafy/farmcu/api/repository/ItemRepository.java
+++ back/farmcu/src/main/java/com/ssafy/farmcu/api/repository/ItemRepository.java
@@ -2,6 +2,7 @@ package com.ssafy.farmcu.api.repository;
 
 import com.ssafy.farmcu.api.entity.store.CategoryDetail;
 import com.ssafy.farmcu.api.entity.store.Item;
+import com.ssafy.farmcu.api.entity.store.Store;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,6 +14,7 @@ import java.util.Optional;
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
     Optional<Item> findByItemId(Long itemId);
+    Slice<Item> findByStore(Store store);
     Slice<Item> findByItemNameLike(String itemName);
     Slice<Item> findByCategoryDetailAndItemNameLike(CategoryDetail categoryDetail, String itemName);
     void deleteByItemId(Long itemId);
