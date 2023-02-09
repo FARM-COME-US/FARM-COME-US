90f04a45507fb0915620395a86873ddb08bbfe8a
JieunLee <jieun3181@gmail.com>
2023-02-09 09:43:25 +0900

    fix : order.controller

--- back/farmcu/src/main/java/com/ssafy/farmcu/api/dto/order/CartDto.java
+++ back/farmcu/src/main/java/com/ssafy/farmcu/api/dto/order/CartDto.java
@@ -14,15 +14,10 @@ public class CartDto {
     @NotNull
     private int cartItemCount;
 
-<<<<<<< HEAD
     @NotNull
     private Long memberId;
 
     @NotNull
-=======
-    private Long memberId;
-
->>>>>>> 9611c9a0c605f1aa899f830650098fa3282b4b3e
     private Long itemId;
 
 //    private Long storeId;   // => 같은 상점끼리 묶어서 보여주기
@@ -51,10 +46,7 @@ public class CartDto {
 //        this.storeName = cart.getItem().getStore().getStoreName();
 //        this.itemSalePercent = cart.getItem().getItemDiscount();
 //        this.getTotalPrice = cartItemCount*cart.getItem().getItemPrice();
-<<<<<<< HEAD
 
-=======
->>>>>>> 9611c9a0c605f1aa899f830650098fa3282b4b3e
     }
 
 
