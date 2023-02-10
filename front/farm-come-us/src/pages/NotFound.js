b5d0c08e83ecf4d421e09a6ea635fea51cf2151f
kevin-Arpe <arpeggiogio@gmail.com>
2023-02-11 01:26:49 +0900

    modify: 라이브 스토어 구매자 뷰 컴포넌트 작성

--- front/farm-come-us/src/pages/NotFound.js
+++ front/farm-come-us/src/pages/NotFound.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import classes from "./NotFound.module.scss";
+import classes from "./style/NotFound.module.scss";
 
 const NotFound = () => {
   return <div className={classes.NotFound}>404, Page NotFound.</div>;
