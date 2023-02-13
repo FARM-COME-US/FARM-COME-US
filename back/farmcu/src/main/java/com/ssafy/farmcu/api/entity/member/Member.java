a5caae40b0511f86bf35e90bd0adf76b45f5bf4f
김지희 <cjg05034@naver.com>
2023-02-14 02:26:02 +0900

    add: 카카오회원가입 추가 정보 api

--- back/farmcu/src/main/java/com/ssafy/farmcu/api/entity/member/Member.java
+++ back/farmcu/src/main/java/com/ssafy/farmcu/api/entity/member/Member.java
@@ -1,5 +1,6 @@
 package com.ssafy.farmcu.api.entity.member;
 
+import com.ssafy.farmcu.api.dto.member.MemberAdditionalReq;
 import com.ssafy.farmcu.api.dto.member.MemberUpdateReq;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
@@ -90,7 +91,14 @@ public class Member {
         this.phoneNumber = memberUpdateReq.getPhoneNumber(); // 휴대폰 번호 인증?
 
     }
+    public void updateAddInfo(MemberAdditionalReq memberUpdateReq){
+        this.name = memberUpdateReq.getName();
+        this.streetAddr = memberUpdateReq.getStreetAddr();
+        this.detailAddr = memberUpdateReq.getDetailAddr();
+        this.zipcode = memberUpdateReq.getZipcode();
+        this.phoneNumber = memberUpdateReq.getPhoneNumber();
 
+    }
 
     public String getRoleType(){
         return this.getRole().toString();
