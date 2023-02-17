f5e33693e3439a7a0b8f8519cb6dea91490f52c1
4priltwntsx <4priltwntsx@naver.com>
2023-02-17 10:34:41 +0900

    Update back/farmcu/src/main/java/com/ssafy/farmcu/api/controller/member/LoginController.java

--- back/farmcu/src/main/java/com/ssafy/farmcu/api/controller/member/LoginController.java
+++ back/farmcu/src/main/java/com/ssafy/farmcu/api/controller/member/LoginController.java
@@ -34,7 +34,7 @@ public class LoginController {
     public String kakaoConnect() throws UnsupportedEncodingException {
         log.info("kakao 로그인 바로가기-  여기 뜨나요:?");
 
-        String REDIRECT_URL = URLEncoder.encode("http://localhost:3000/login/oauth2/code/kakao", "UTF-8");
+        String REDIRECT_URL = URLEncoder.encode("https://localhost:3000/login/oauth2/code/kakao", "UTF-8");
         SecureRandom secureRandom = new SecureRandom();
         String state = new BigInteger(130, secureRandom).toString();
         String apiURL = "https://kauth.kakao.com/oauth/authorize?response_type=code";
@@ -50,7 +50,7 @@ public class LoginController {
     public void getAccessToken(@RequestParam(value = "code") String code, @RequestParam(value = "state") String state, HttpServletResponse response) throws UnsupportedEncodingException {
         log.info("토큰 발급/갱신/삭제 요청 URL");
 
-        String REDIRECT_URL = URLEncoder.encode("http://localhost:3000/login/oauth2/code/kakao", "UTF-8");
+        String REDIRECT_URL = URLEncoder.encode("https://localhost:3000/login/oauth2/code/kakao", "UTF-8");
         String apiURL = "https://kauth.kakao.com/oauth/token?grant_type=authorization_code&";
         apiURL += "client_id=" + CLIENT_ID;
         apiURL += "&client_secret=" + CLIENT_SECRET;
